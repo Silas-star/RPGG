7f1b9bc188dd30fff801e71378d6a8c669666b44
Silas-star <kauavefago20@gmail.com>
2021-02-06 10:03:26 -0300

    First commit

---
+++ src/silasmenu.js
@@ -0,0 +1,12 @@
+const Silasmenu = (prefix, pushname) => {
+    return `❍ *Comandos do silas*
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.silasmenu = silasmenu
